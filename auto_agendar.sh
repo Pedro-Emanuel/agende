3ddbaff7aead9e0e7b1b8ce2d855894ec305037d
Pedro-Emanuel <pedroems.147@gmail.com>
2024-08-18 22:50:57 -0300

    Removing my absolute path from shell script :P

--- auto_agendar.sh
+++ auto_agendar.sh
@@ -4,9 +4,9 @@ if [ -f ".env" ]; then
     export $(grep -v '^#' .env | xargs)
 fi
 
-PYTHON_SCRIPT="/home/pedro/git-workspace/agendar_ru/agendar.py"
+PYTHON_SCRIPT="./agendar.py"
 DATA_AMANHA=$(date -d "tomorrow" +"%d/%m/%Y")
-LOG_FILE="/home/pedro/git-workspace/agendar_ru/output.log"
+LOG_FILE="./output.log"
 
 {
     echo -n "[$(date +'%d/%m/%Y %H:%M:%S')] (Almoço) "
@@ -15,4 +15,6 @@ LOG_FILE="/home/pedro/git-workspace/agendar_ru/output.log"
     echo -n "[$(date +'%d/%m/%Y %H:%M:%S')] (Jantar) "
     python3 "$PYTHON_SCRIPT" "$USERNAME" "$PASSWORD" "$DATA_AMANHA" "3" >> "$LOG_FILE" 2>&1
 } >> "$LOG_FILE" 2>&1
+
+echo >> "$LOG_FILE"
 
